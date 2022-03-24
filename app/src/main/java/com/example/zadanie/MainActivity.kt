81bf8680ccb7fec06d78bbd2e55a1ab85c9f11b4
Mrblaszka <mateosblaszka@outlook.com>
2022-03-24 12:24:09 +0100

    Wygląd + 2 zmienne

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val men = findViewById<RadioButton>(R.id.m)
+            if(men.isSelected){
+                findViewById<TextView>(R.id.lista).text = "Mężczyzna"
+            }
+        val grl = findViewById<RadioButton>(R.id.k)
+            if(grl.isSelected){
+                findViewById<TextView>(R.id.lista).text = "Kobieta"
+            }
+
+
     }
 }
